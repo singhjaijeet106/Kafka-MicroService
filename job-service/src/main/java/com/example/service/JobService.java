824e8f1620e33d48f2e1be50b41a33a3a0034faf
singhjaijeet106 <singhjaijeet106@gmail.com>
2024-08-20 21:28:03 -0400

    Feign Client  in Job service
    Implemented and added Feign Client  in Job service

--- job-service/src/main/java/com/example/service/JobService.java
+++ job-service/src/main/java/com/example/service/JobService.java
@@ -3,7 +3,12 @@ package com.example.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import com.example.client.CompanyServiceClient;
+import com.example.model.ApiResponse;
+import com.example.model.CompanyResponse;
 import com.example.model.JobResponse;
 import com.example.repo.JobRepositoryLogic;
 
@@ -16,6 +21,9 @@ public class JobService {
 	@Autowired
 	private JobRepositoryLogic jobRepository;
 	
+	@Autowired
+	private CompanyServiceClient companyClient;
+	
 	public List<JobResponse> getAllJobsList(){
 		List<JobResponse> jobResponse = jobRepository.getAllJobResponse();
 		return jobResponse;
@@ -23,7 +31,8 @@ public class JobService {
 	
 	public JobResponse getJobsById(String jobId){
 		JobResponse jobResponse = jobRepository.getJobResponseByJobId(jobId);
-		
+		CompanyResponse companyResponse = getCompanyDetailByCompanyId(jobResponse.getCompany().getId());
+		jobResponse.setCompany(companyResponse);
 		return jobResponse;
 	}
 	
@@ -41,6 +50,13 @@ public class JobService {
 	public boolean updateJobDetails(String id,JobResponse jobResponse){
 		boolean response = jobRepository.updateJobdetailsById(id, jobResponse);
 		return response;
+	}
+	
+	private CompanyResponse getCompanyDetailByCompanyId(String companyId) {
+		CompanyResponse companyResponse=null;
+		ApiResponse<CompanyResponse> apiResponse = companyClient.getCompanyById(companyId).getBody();
+		companyResponse = apiResponse.getResponseInfo();
+		return companyResponse;
 	}
 	
 }
